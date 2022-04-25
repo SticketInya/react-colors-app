159fca8c2ecf6d981bcd0590e970146be1c2936c
JPatrik <jokhel.roland@gmail.com>
2022-04-25 15:14:48 +0200

    feat(Navbar.js): add UI notification for format change

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,26 +3,44 @@ import { Link } from 'react-router-dom';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
-import { MenuItem, Select } from '@mui/material';
+import { MenuItem, Select, Snackbar } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
             format: 'hex',
+            isOpen: false,
         };
     }
 
     handleChange = (e) => {
         const newFormat = e.target.value;
-        this.setState({ format: newFormat }, () => {
+        this.setState({ format: newFormat, isOpen: true }, () => {
             this.props.changeFormat(newFormat);
         });
     };
 
+    handleClose = () => {
+        this.setState({ isOpen: false });
+    };
+
     render() {
         const { paletteName, colorLevel, handleSliderChange } = this.props;
-        const { format } = this.state;
+        const closeButton = (
+            <>
+                <IconButton
+                    size='small'
+                    aria-label='close'
+                    color='inherit'
+                    onClick={this.handleClose}
+                >
+                    <CloseIcon fontSize='small' />
+                </IconButton>
+            </>
+        );
         return (
             <nav className='Navbar'>
                 <div className='Navbar__title'>
@@ -46,6 +64,14 @@ class Navbar extends Component {
                         <MenuItem value='rgba'>RGBA</MenuItem>
                     </Select>
                 </div>
+                <Snackbar
+                    open={this.state.isOpen}
+                    autoHideDuration={2000}
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                    onClose={this.handleClose}
+                    message={'Format changed!'}
+                    action={closeButton}
+                />
             </nav>
         );
     }
