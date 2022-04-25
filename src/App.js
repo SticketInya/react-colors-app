91eb4b692a4846dbe8f3c43e335b519785d57e3c
JPatrik <jokhel.roland@gmail.com>
2022-04-25 16:03:56 +0200

    feat(App.js): add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import generatePalette from './Helpers/ColorHelper';
-import ColorPalette from './Components/ColorPalette/ColorPalette';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 import DefaultColors from './Components/DefaultColors';
+import FindPalette from './Helpers/FindPalette';
 
 function App() {
     return (
         <div className='App'>
-            <ColorPalette palette={generatePalette(DefaultColors[0])} />
+            <Routes>
+                <Route exact path={'/'} element={<h1>Home</h1>} />
+                <Route
+                    exact
+                    path={'/palette/:id'}
+                    element={<FindPalette colorPalettes={DefaultColors} />}
+                />
+                <Route path={'*'} element={<Navigate to='/' replace />} />
+            </Routes>
         </div>
     );
 }
