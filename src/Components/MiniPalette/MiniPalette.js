0e18d342044e75caa0963b31f924824599f2e312
JPatrik <jokhel.roland@gmail.com>
2022-04-28 15:31:03 +0200

    feat(MiniPalette): add dialog conformation to palette delete

--- src/Components/MiniPalette/MiniPalette.js
+++ src/Components/MiniPalette/MiniPalette.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 //Mui
 import DeleteIcon from '@mui/icons-material/Delete';
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import PersonIcon from '@mui/icons-material/Person';
+import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
+import { blue } from '@mui/material/colors';
 
 import './MiniPalette.css';
 
@@ -13,6 +26,7 @@ export default function MiniPalette({
     id,
     deletePalette,
 }) {
+    const [dialogOpen, setDialogOpen] = useState(false);
     const navigate = useNavigate();
     const colorBoxes = colors.map((color) => {
         return (
@@ -28,24 +42,48 @@ export default function MiniPalette({
         navigate(`/palette/${id}`);
     };
 
-    const handleDelete = (e) => {
-        e.stopPropagation();
+    const handleDelete = () => {
         deletePalette(id);
     };
 
+    const handleClose = () => {
+        setDialogOpen(false);
+    };
+
+    const handleClick = (e) => {
+        e.stopPropagation();
+        setDialogOpen(true);
+    };
+
     return (
-        <div className='MiniPalette' onClick={goToPalette}>
-            <DeleteIcon
-                onClick={handleDelete}
-                className='MiniPalette__delete'
-            />
-            <div className='MiniPalette__container'>
-                <div className='MiniPalette__colors'>{colorBoxes}</div>
-                <h2 className='MiniPalette__title'>
-                    {paletteName}
-                    <span className='emoji'>{emoji}</span>
-                </h2>
+        <>
+            <div className='MiniPalette' onClick={goToPalette}>
+                <DeleteIcon
+                    onClick={handleClick}
+                    className='MiniPalette__delete'
+                />
+                <div className='MiniPalette__container'>
+                    <div className='MiniPalette__colors'>{colorBoxes}</div>
+                    <h2 className='MiniPalette__title'>
+                        {paletteName}
+                        <span className='emoji'>{emoji}</span>
+                    </h2>
+                </div>
             </div>
-        </div>
+            <Dialog onClose={handleClose} open={dialogOpen}>
+                <DialogTitle>
+                    Would you like to delete this Palette?
+                </DialogTitle>
+                <List sx={{ pt: 0 }}>
+                    <ListItem button onClick={handleDelete} key={'bnt-delete'}>
+                        <ListItemText primary='Delete' />
+                    </ListItem>
+
+                    <ListItem button onClick={handleClose} key={'btn-cancel'}>
+                        <ListItemText primary='Cancel' />
+                    </ListItem>
+                </List>
+            </Dialog>
+        </>
     );
 }
